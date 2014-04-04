e515fa765467fc3d675d14da5e22a044b6248adc
refaktorco <refaktor.co@gmail.com>
2014-04-04 11:12:49 -0700

    -Working on portfolio stuff.

---
+++ templates/node--view--portfolio.tpl.php
@@ -0,0 +1,17 @@
+<li class="grid-item design development">
+	<div class="grid-project">
+		<!-- ajax link -->
+		<a href="#!projects/project-1.html" class="open-slide">
+		<!-- project image -->
+		<div class="img-box">
+			<img src="preview/p.jpg" alt="project">
+		</div>
+		<!-- project title and category -->
+		<div class="project-info">
+			<h1><?php print $title; ?></h1>
+			<h6>Design &amp; Development</h6>
+		</div>
+		</a>
+	</div>
+</li>
+
--- templates/node--view--skills-and-services.tpl.php
+++ templates/node--view--skills-and-services.tpl.php
@@ -7,6 +7,7 @@
 	</div>
 	<?php endif; ?>	
 	<!-- service info -->
+	<h5 class="s-bold"><?php print $title; ?></h5>
 	<p>
 	<?php
     $teaser = strip_tags(render($content['body']));
