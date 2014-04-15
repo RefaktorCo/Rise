fe002ba428d7797fcbee4b2c396808d8e9c4c7e5
refaktorco <refaktor.co@gmail.com>
2014-04-15 13:25:06 -0700

    -Video working well, just a couple tweaks needed. Need to implement some kind of setting or field option to control this.

--- templates/node/node--parallax.tpl.php
+++ templates/node/node--parallax.tpl.php
@@ -1,11 +1,12 @@
 <style>
 .parallax-<?php print $node->nid; ?>{
-  background:url(<?php echo file_create_url($node->field_parallax_background['und'][0]['uri']); ?>) no-repeat center center fixed;
-  
+  background:url(<?php echo file_create_url($node->field_parallax_background['und'][0]['uri']); ?>);
+  background-attachment:fixed;
+  background-size:cover;
 }
 </style>
 
-<div class="parallax-<?php print $node->nid; ?> parallax" >
+<div class="parallax-<?php print $node->nid; ?>" data-stellar-background-ratio="0.5">
 	<div class="tint largepadding">
 		<!-- section heading -->
 		<section class="row heading">
