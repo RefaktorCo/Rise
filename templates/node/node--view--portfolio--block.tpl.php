a146a0a7158735b05184f8edbc94ce2dd49f0231
refaktorco <refaktor.co@gmail.com>
2014-07-03 14:43:46 -0700

    To be continued

--- templates/node/node--view--portfolio--block.tpl.php
+++ templates/node/node--view--portfolio--block.tpl.php
@@ -9,7 +9,7 @@
 		<!-- project title and category -->
 		<div class="project-info">
 			<h1><?php print $title; ?></h1>
-			<h6><?php print render($content['field_portfolio_category']); ?></h6>
+			<h6><?php print str_replace(' ', ' & ',strip_tags(render($content['field_portfolio_category']))); ?></h6>
 		</div>
 		</a>
 	</div>
